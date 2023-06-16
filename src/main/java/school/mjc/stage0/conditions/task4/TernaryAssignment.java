67a294be6eaed7e1a4492282500ff636e9e131c4
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-16 00:11:26 -0400

    Update TernaryAssignment.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result;
+        int b = first < second ?  ( result = second): (result= first);
+        System.out.println(result);
     }
 }
