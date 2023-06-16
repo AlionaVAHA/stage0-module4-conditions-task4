65bbc4646ad965acdcd8eec0c9e7e803ac7a0675
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 23:53:42 -0400

    Update TernaryGreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int greates;
+        int b = first < second ?  ( greates = second): (greates= first);
+        System.out.println(greates);
     }
 }
